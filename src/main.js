5b8ca7e7017b079aa93b767476966467a98d44dc
Shin Oka <shinoka7@gmail.com>
2024-08-11 13:20:45 -0700

    display with datatable

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-
-// Vuetify
 import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+import vuetify from "./plugins/vuetify.js";
 
-createApp(App).use(vuetify).mount("#app");
+const app = createApp(App);
+app.use(vuetify);
+app.mount("#app");
