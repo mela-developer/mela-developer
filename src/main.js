fc49ec6d6030dabe75a82cc2e0ce734e868c5e1f
Shin Oka <shinoka7@gmail.com>
2024-08-11 13:24:15 -0700

    Updates

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import "vuetify/styles";
-import vuetify from "./plugins/vuetify.js";
-
-const app = createApp(App);
-app.use(vuetify);
-app.mount("#app");
